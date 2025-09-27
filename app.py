361bfa743880cba01e4a3fab622be903624a0975
Bladi1802 <bladihernandez1802@gmail.com>
2025-09-26 19:52:46 -0700

    Agregué app.py con la descripción del proyecto e integrantes

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # Página base para verificar que la app corre y se renderiza correctamente
+    return render_template("index.html", titulo="be-equipo2-biblioteca API", mensaje="¡Bienvenido a be-equipo2-biblioteca!")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
